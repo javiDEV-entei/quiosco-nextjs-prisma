a53b90bb2f151143ba77fef49d47ae4a0a87d6ca
javiDEV-entei <javieroliveradev0239@gmail.com>
2026-02-23 11:47:46 -0300

    BD creada y configurada y modelos agregados
    se creo la BD con prisma y se agregaron los modelos para categorias y prodctos

---
+++ prisma/seed.ts
@@ -0,0 +1,42 @@
+import { categories } from "./data/categories";
+import { products } from "./data/products";
+import {PrismaClient, Prisma} from '@/src/generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import 'dotenv/config'
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+})
+
+const prisma = new PrismaClient({
+    adapter,
+})
+
+async function main(){
+    try {
+        await prisma.category.createMany({
+            data: categories
+        })
+        await prisma.product.createMany({
+            data: products
+        })
+        console.log('Seed Ejecutado Correctamente');
+        
+
+    } catch (error) {
+        console.log(error);
+        
+        
+    }
+}
+
+main()
+    .then(async() =>{
+        await prisma.$disconnect()
+    })
+    .catch(async(e)=>{
+        console.log(e)
+        await prisma.$disconnect()
+        process.exit(1)
+        
+    })
